1b5fa3dd16b8757c0e8bf8363faef6d4edf68d7f
thotasailakshmi532 <thotasailakshmi532@gmail.com>
2023-11-01 12:19:53 +0530

    Add project files.

---
+++ StoringAndUpdateRecordLibrary/TeacherDetails.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoringAndUpdateRecordLibrary
+{
+    public class TeacherDetails
+    {
+        private const string FilePath = "D:\\Practices Exercise\\.net\\MyEndProject2\\Teachers.txt";
+
+        public static List<Teacher> GetAllTeachers()
+        {
+            List<Teacher> teachers = new List<Teacher>();
+
+            if (File.Exists(FilePath))
+            {
+                using (StreamReader reader = new StreamReader(FilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(',');
+                        teachers.Add(new Teacher
+                        {
+                            ID = int.Parse(data[0]),
+                            Name = data[1],
+                            ClassAndSection = data[2]
+                        });
+                    }
+                }
+            }
+
+            return teachers;
+        }
+
+        public static void AddTeacher(Teacher teacher)
+        {
+            using (StreamWriter writer = new StreamWriter(FilePath, true))
+            {
+                writer.WriteLine($"{teacher.ID},{teacher.Name},{teacher.ClassAndSection}");
+            }
+        }
+
+        // Add methods for updating and deleting teachers if needed
+    }
+}
+
---
+++ StoringAndUpdateRecordProject/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoringAndUpdateRecordLibrary;
+
+namespace StoringAndUpdateRecordProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Teacher Management System");
+
+            do
+            {
+                Console.WriteLine("1. Add Teacher");
+                Console.WriteLine("2. View All Teachers");
+                Console.WriteLine("3. Exit");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        // Add Teacher
+                        Console.WriteLine("Enter Teacher ID:");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter Teacher Name:");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Enter Class and Section:");
+                        string classAndSection = Console.ReadLine();
+
+                        Teacher newTeacher = new Teacher { ID = id, Name = name, ClassAndSection = classAndSection };
+                        TeacherDetails.AddTeacher(newTeacher);
+                        Console.WriteLine("Teacher added successfully!");
+                        break;
+
+                    case 2:
+                        // View All Teachers
+                        List<Teacher> teachers = TeacherDetails.GetAllTeachers();
+                        foreach (var teacher in teachers)
+                        {
+                            Console.WriteLine($"{teacher.ID}, {teacher.Name}, {teacher.ClassAndSection}");
+                        }
+                        break;
+
+                    case 3:
+                        // Exit the program
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine("Press Enter to continue...");
+                Console.ReadLine();
+                Console.Clear();
+
+            } while (true);
+        }
+    }
+}
